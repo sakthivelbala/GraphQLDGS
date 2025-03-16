5d48895606ac2d8ec3938fbf15688663be42ea47
Sakthivel Balakrishnan <sakthivelbalakrishnan@gmail.com>
2025-03-16 20:32:11 +0530

    initial commit

---
+++ src/test/java/com/codewithsakthi/graphqldgs/GraphqldgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithsakthi.graphqldgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqldgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
